730d0ede8959ebe39f4d4d995b0d9a3270491770
Анжела <anzhela.namistyuk@yandex.ru>
2022-07-11 17:51:19 +0300

    6-th commit

--- settings.py
+++ settings.py
@@ -1,11 +1,7 @@
 import os
 
 
-def getenv():
-    return 'sqlite:///db.sqlite3'
-
-
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
