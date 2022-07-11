96a11055864316d4c6fffa7d44d5e3d63e67d2b3
Анжела <anzhela.namistyuk@yandex.ru>
2022-07-11 17:24:28 +0300

    4-d commit

--- settings.py
+++ settings.py
@@ -3,5 +3,5 @@ import os
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
